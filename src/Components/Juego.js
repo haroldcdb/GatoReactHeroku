2c298b6aa46211d7f3ed89222db49d2abde087c2
haroldcdb2301 <cordobaharold500@gmail.com>
2021-03-14 21:15:19 -0600

    testing

--- src/Components/Juego.js
+++ src/Components/Juego.js
@@ -1,102 +1,112 @@
 import React, {
-    useState, useEffect
-  } from 'react';
+  useState, useEffect
+} from 'react';
 
 import Tablero from "./Tablero";
 
 const Juego = () => {
 
-    const [historial, setHistorial] = useState([{
-      cuadrados: Array(9).fill(null)
-    }]);
-    const [pasoNumero, setPasoNumero] = useState(0);
-    const [XesSiguiente, setXesSiguiente] = useState(true);
-  
-    const handleClick = (i) => {
-      let historialCopy = historial.slice(0, pasoNumero + 1);
-      const concurrencia = historialCopy[historialCopy.length - 1]
-      const cuadros = concurrencia.cuadrados.slice();
-      if (calculateWinner(cuadros) || cuadros[i]) {
-        return;
-      }
-      cuadros[i] = XesSiguiente ? 'X' : 'O';
+  useEffect(() => {
+    window.axios.get('/randomValue')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, [])
 
-      setHistorial(historialCopy.concat([{
-        cuadrados: cuadros,
-      }]));
-      setPasoNumero(historialCopy.length);
-      setXesSiguiente(!XesSiguiente);
-    }
+  const [historial, setHistorial] = useState([{
+    cuadrados: Array(9).fill(null)
+  }]);
+  const [pasoNumero, setPasoNumero] = useState(0);
+  const [XesSiguiente, setXesSiguiente] = useState(true);
 
-    const jumpTo = (paso) => {
-      setPasoNumero(paso);
-      setXesSiguiente((paso % 2) === 0);
+  const handleClick = (i) => {
+    let historialCopy = historial.slice(0, pasoNumero + 1);
+    const concurrencia = historialCopy[historialCopy.length - 1]
+    const cuadros = concurrencia.cuadrados.slice();
+    if (calculateWinner(cuadros) || cuadros[i]) {
+      return;
     }
-  
-    const hist = historial;
-    const concurrencia = hist[pasoNumero];
-    const ganador = calculateWinner(concurrencia.cuadrados);
-  
-    const movimientos = hist.map((paso, movimiento) => {
-      const desc = movimiento ?
-        'Ir al moviento #' + movimiento :
-        'Iniciar nuevo juego';
-      return (
-        <li key={movimiento}>
-          <button onClick={() => jumpTo(movimiento)}>{desc}</button>
-        </li>
-      );
-  
-    });
-  
-  
-    let estatus;
-    if (ganador) {
-      estatus = 'Ganador: ' + ganador;
-    } else {
-      estatus = 'Siguiente jugador: ' + (XesSiguiente ? 'X' : 'O');
-    }
-  
-  
+    cuadros[i] = XesSiguiente ? 'X' : 'O';
+
+    setHistorial(historialCopy.concat([{
+      cuadrados: cuadros,
+    }]));
+    setPasoNumero(historialCopy.length);
+    setXesSiguiente(!XesSiguiente);
+  }
+
+  const jumpTo = (paso) => {
+    setPasoNumero(paso);
+    setXesSiguiente((paso % 2) === 0);
+  }
+
+  const hist = historial;
+  const concurrencia = hist[pasoNumero];
+  const ganador = calculateWinner(concurrencia.cuadrados);
+
+  const movimientos = hist.map((paso, movimiento) => {
+    const desc = movimiento ?
+      'Ir al moviento #' + movimiento :
+      'Iniciar nuevo juego';
     return (
-      <div className="game">
-        <div className="game-board">
-          <Tablero
-            cuadrados={concurrencia.cuadrados}
-            /*onClick={(i) =>{
-            return  handleClick(i)
-            } }*/
-            onClick={i =>  handleClick(i) }/*lo mismo de arriba pero simplificado */
-          />
-        </div>
-        <div className="game-info">
-          <div>{estatus}</div>
-          <ol>{movimientos}</ol>
-        </div>
-      </div>
+      <li key={movimiento}>
+        <button onClick={() => jumpTo(movimiento)}>{desc}</button>
+      </li>
     );
-  
+
+  });
+
+
+  let estatus;
+  if (ganador) {
+    estatus = 'Ganador: ' + ganador;
+  } else {
+    estatus = 'Siguiente jugador: ' + (XesSiguiente ? 'X' : 'O');
   }
 
-  
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Tablero
+          cuadrados={concurrencia.cuadrados}
+          /*onClick={(i) =>{
+          return  handleClick(i)
+          } }*/
+          onClick={i => handleClick(i)}/*lo mismo de arriba pero simplificado */
+        />
+      </div>
+      <div className="game-info">
+        <div>{estatus}</div>
+        <ol>{movimientos}</ol>
+      </div>
+    </div>
+  );
+
+}
+
+
 function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null;
   }
+  return null;
+}
 
 export default Juego;
--- src/index.js
+++ src/index.js
@@ -7,39 +7,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Juego from "./Components/Juego";
 
-const apiUrl = (url) => {
-  if (_.isUndefined(url) || _.isNull(url)) {
-    return '';
-  }
-  if (url.indexOf('https://') >= 0 || url.indexOf('http://') >= 0) {
-    return url;
-  }
-  if (url.indexOf('/') !== 0) {
-    url = '/' + url;
-  }
-
-  return 'https://media.4thewords.com/s3' + url;
-}
-
 const protocol = window.location.protocol;
 const location = window.location.hostname;
-
-console.log(protocol);
-console.log(location);
-
 if (location.includes('localhost')) {
   window.apiDomain = protocol + '//localhost:3030';
   window.publicDomain = protocol +'//localhost:3000';
-} else if (location.includes('develop')) {
+} else if (location.includes('dev')) {
   window.apiDomain = protocol + '//gato-backend-dev.herokuapp.com';
   window.publicDomain = protocol + '//gato-react-heroku-dev.herokuapp.com';
 } else {
-  window.apiDomain = 'https://gato-backend.herokuapp.com';
+  window.apiDomain = protocol + '//gato-backend.herokuapp.com';
   window.publicDomain = protocol + '//gato-react-heroku.herokuapp.com';
 }
-
-console.log(window.apiDomain);
-console.log(window.publicDomain);
 
 window.axios = axios.create({
   headers: {
